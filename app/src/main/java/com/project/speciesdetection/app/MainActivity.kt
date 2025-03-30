844380a3fe06386b7042d9e0c5a25b90118f012d
P <haiphungw0@gmail.com>
2025-03-30 16:01:08 +0700

    refactoring file path & adding splashscreen

--- app/src/main/java/com/project/speciesdetection/app/MainActivity.kt
+++ app/src/main/java/com/project/speciesdetection/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.project.speciesdetection
+package com.project.speciesdetection.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,37 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.project.speciesdetection.ui.theme.SpeciesDetectionTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.project.speciesdetection.core.theme.SpeciesDetectionTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             SpeciesDetectionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SpeciesDetectionTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/project/speciesdetection/data/model/user/User.kt
@@ -0,0 +1,9 @@
+package com.project.speciesdetection.data.model.user
+
+data class User(
+    val id: String,
+    val name: String,
+    val email: String,
+    val imageURL: String
+) {
+}
