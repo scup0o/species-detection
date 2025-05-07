6cee11eaa28e24b63142d4c183ad56a507147d8f
P <haiphungw0@gmail.com>
2025-05-07 17:50:40 +0700

    commit checkpoint before update

--- app/src/main/java/com/project/speciesdetection/data/model/user/repository/UserRepositoryModule.kt
+++ app/src/main/java/com/project/speciesdetection/data/model/user/repository/UserRepositoryModule.kt
@@ -3,9 +3,10 @@ package com.project.speciesdetection.data.model.user.repository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn
+@InstallIn(SingletonComponent::class)
 abstract class UserRepositoryModule {
     @Binds
     abstract fun bindRemoteUserRepository(remoteRepository: RemoteUserRepository) : UserRepository
